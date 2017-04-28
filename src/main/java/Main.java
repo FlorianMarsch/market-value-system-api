694a214877a7ccfeab65f5c1aba04462c68d6a2a
Florian <dekontaminator@googlemail.com>
2017-04-28 20:22:45 +0200

    initial redis

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,65 @@
 import static spark.Spark.get;
+import static spark.Spark.put;
 import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-import spark.ModelAndView;
-import spark.template.freemarker.FreeMarkerEngine;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		
+		String REDIS_URL = System.getenv("REDIS_URL");
+		if (REDIS_URL == null) {
+			throw new RuntimeException("No REDIS_URL found");
+		}
+
+		JedisPool pool;
+		try {
+			System.out.println("Init REDIS :"+REDIS_URL);
+			pool = getPool(new URI(REDIS_URL));
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Bad REDIS_URL", e);
+		}
+
 		String port = System.getenv("PORT");
-		if(port == null || port.isEmpty()){
-			port ="80";
+		if (port == null || port.isEmpty()) {
+			port = "80";
 		}
 		port(Integer.valueOf(port));
 		staticFileLocation("/public");
 
-		get("/api/test/:id", (request, response) -> {
+		get("/api/:key", (request, response) -> {
+			String key = request.params(":key");
+			Jedis jedis = pool.getResource();
+			String value = jedis.get(key);			
+			return value;
+		});
+		
+		put("/api/:key", (request, response) -> {
+			String key = request.params(":key");
+			String value = request.body();
+			Jedis jedis = pool.getResource();
+			String statusCode = jedis.set(key, value);
+			return statusCode;
+		});
+	}
 
-			String id = request.params(":id");
-			Map<String, Object> attributes = new HashMap<>();
-			attributes.put("data","Hello "+ id);
-			return new ModelAndView(attributes, "json.ftl");
-		} , new FreeMarkerEngine());
+	public static JedisPool getPool(URI aURL) {
+		JedisPoolConfig poolConfig = new JedisPoolConfig();
+		poolConfig.setMaxTotal(10);
+		poolConfig.setMaxIdle(5);
+		poolConfig.setMinIdle(1);
+		poolConfig.setTestOnBorrow(true);
+		poolConfig.setTestOnReturn(true);
+		poolConfig.setTestWhileIdle(true);
+		JedisPool pool = new JedisPool(poolConfig, aURL);
+		return pool;
 	}
 
 }
